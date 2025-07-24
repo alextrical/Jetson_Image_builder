27501a4d242db8195792188e2aea56149de875be
alextrical <35117191+alextrical@users.noreply.github.com>
2025-07-24 20:16:08 +0100

    Testing Workflow Dispatch Variables

---
+++ scripts/install_packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+
+sudo apt purge -y \
+  libreoffice* printer* ubiquity* mono* python2* \
+  --auto-remove
+
+sudo apt autoremove -y
+
+apt-get upgrade -y
+# apt-get install -y \
+#     curl \
+#     wget \
+#     git \
+#     vim \
+#     htop \
+#     build-essential \
+#     cmake \
+#     python3-dev \
+#     python3-pip \
+#     python3-setuptools \
+#     nodejs \
+#     npm \
+#     docker.io \
+#     openssh-server \
+#     can-utils \
+#     i2c-tools \
+#     net-tools \
+#     wireless-tools \
+#     network-manager
+
+# # Install Python packages
+# pip3 install \
+#     numpy \
+#     opencv-python \
+#     flask \
+#     requests \
+#     pyyaml
+
+# Clean up
+apt-get clean
+rm -rf /var/lib/apt/lists/*
